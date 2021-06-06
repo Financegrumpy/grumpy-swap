46dbad2f8d53a9e8bcd50f32dbda289aeb2db544
Joe Lum <lumtwj@gmail.com>
2021-06-06 14:48:37 +0800

    Added current $grumpy price from ethplorer

--- src/pages/Stats/index.tsx
+++ src/pages/Stats/index.tsx
@@ -44,28 +44,75 @@ const MainContentWrapper = styled.main`
   flex-direction: column;
 `
 const PaddedAutoColumn = styled(AutoColumn)`
-  padding: 10px;
+  padding: 12px;
 `
 
+const ethescanApiKey = 'SZYGYXBA7K6ECH7DHB3QX2MR7GJZQK2M8P'
+const ethplorerApiKey = 'freekey'
 const grumpyContractAddress = '0x93b2fff814fcaeffb01406e80b4ecd89ca6a021b';
 
 export default function Stats() {
   const { account, chainId } = useActiveWeb3React()
 
   //
-  const [grumpyBalance, setGrumpyBalance] = useState('-')
-  const [grumpyBalanceWithoutRedistribution, setGrumpyBalanceWithoutRedistribution] = useState('-')
-  const [redistributedAmount, setRedistributedAmount] = useState('-')
-  const [totalIn, setTotalIn] = useState('-')
-  const [totalOut, setTotalOut] = useState('-')
+  const [grumpyBalance, setGrumpyBalance] = useState(0)
+  const [grumpyBalanceWithoutRedistribution, setGrumpyBalanceWithoutRedistribution] = useState(0)
+  const [redistributedAmount, setRedistributedAmount] = useState(0)
+  const [totalIn, setTotalIn] = useState(0)
+  const [totalOut, setTotalOut] = useState(0)
+  const [price, setPrice] = useState('-')
+  const [marketCap, setMarketCap] = useState('-')
+  const [grumpyUsdValue, setGrumpyUsdValue] = useState('-')
   //
 
   function formatPrice(price: number) {
     if (price > 0) {
-      return (price / 1000000000).toLocaleString().slice(0, -4)
+      const priceString = (price / 1000000000).toLocaleString()
+      const dotIndex = priceString.indexOf('.')
+      return priceString.substring(0, dotIndex)
     }
 
     return price.toString()
+  }
+
+  function formatPriceUsd(price: number) {
+    if (price > 0) {
+      return (price / 1000000000).toLocaleString()
+    }
+
+    return price.toString()
+  }
+
+  async function getGrumpyStats(balance: number) {
+    const stats_api = new URL("https://api.ethplorer.io/getTokenInfo/0x93b2fff814fcaeffb01406e80b4ecd89ca6a021b")
+    stats_api.searchParams.append("apiKey", ethplorerApiKey)
+
+    const statsReq = await fetch(stats_api.href)
+    const statsRes = await statsReq.json()
+
+    if (!statsRes.hasOwnProperty('error')) {
+      const price = statsRes.price
+      const userGrumpyValueInUsd = balance * price.rate
+
+      setPrice('$' + price.rate.toFixed(11))
+      setMarketCap('$' + price.marketCapUsd.toLocaleString())
+      setGrumpyUsdValue('$' + formatPriceUsd(userGrumpyValueInUsd))
+    }
+  }
+
+  async function get() {
+    if (account) {
+      const balance = await getGrumpyBalance(account)
+      const tx = await getGrumpyTransaction(account, balance)
+      getGrumpyStats(balance)
+
+      setGrumpyBalance(balance)
+
+      setTotalIn(tx.totalIn)
+      setTotalOut(tx.totalOut)
+      setRedistributedAmount(tx.redistribution)
+      setGrumpyBalanceWithoutRedistribution(tx.balanceWithoutRedistribution)
+    }
   }
 
   async function getGrumpyBalance(account: string) {
@@ -76,15 +123,16 @@ export default function Stats() {
     balance_api.searchParams.append("contractaddress", grumpyContractAddress)
     balance_api.searchParams.append("address", account)
     balance_api.searchParams.append("tag", "latest")
-    balance_api.searchParams.append("apikey", "SZYGYXBA7K6ECH7DHB3QX2MR7GJZQK2M8P")
+    balance_api.searchParams.append("apikey", ethescanApiKey)
 
     const balanceReq = await fetch(balance_api.href)
     const balanceRes = await balanceReq.json()
     const balance = parseFloat(balanceRes.result)
-    console.log('Your grumpy balance is: ', balance)
-    setGrumpyBalance(formatPrice(balance))
 
-    //
+    return balance
+  }
+
+  async function getGrumpyTransaction(account: string, balance: number) {
     const transactions_api = new URL("https://api.etherscan.io/api")
     transactions_api.searchParams.append("module", "account")
     transactions_api.searchParams.append("action", "tokentx")
@@ -92,6 +140,7 @@ export default function Stats() {
     transactions_api.searchParams.append("address", account)
     transactions_api.searchParams.append("page", "1")
     transactions_api.searchParams.append("offset", "10000")
+    transactions_api.searchParams.append("apikey", ethescanApiKey)
 
     const transactionReq = await fetch(transactions_api.href)
     const transactionRes = await transactionReq.json()
@@ -114,17 +163,12 @@ export default function Stats() {
     const balanceWithoutRedistribution = totalIn - totalOut
     const redistribution = (balance - balanceWithoutRedistribution);
 
-    setTotalIn(formatPrice(totalIn))
-    setTotalOut(formatPrice(totalOut))
-    setRedistributedAmount(formatPrice(redistribution))
-    setGrumpyBalanceWithoutRedistribution(formatPrice(balanceWithoutRedistribution))
-
-    // return fetch(etherscan_url.href);
+    return { totalIn, totalOut, redistribution, balanceWithoutRedistribution }
   }
 
   useEffect(() => {
-    getGrumpyBalance((account) ? account : '')
-  }, [])
+    get()
+  }, [account])
 
   // toggle for showing delegation modal
   const showDelegateModal = useModalOpen(ApplicationModal.DELEGATE)
@@ -167,51 +211,86 @@ export default function Stats() {
         </InfoCard>
       </TopSection>
 
-      <TopSection gap="2px">
-        <WrapSmall>
-          <TYPE.mediumHeader style={{ margin: '0.5rem 0.5rem 0.5rem 0', flexShrink: 0 }}>Wallet</TYPE.mediumHeader>
-        </WrapSmall>
-        <MainContentWrapper>
-          <AutoColumn gap="lg" justify="center">
-            <img src={logo} alt="Logo" style={{ width: 100, height: 100, padding: 20 }} />
-          </AutoColumn>
-          <AutoColumn gap="sm">
-            <TYPE.body textAlign="center">Your $GRUMPY Balance</TYPE.body>
-            <TYPE.largeHeader textAlign="center">{grumpyBalance}</TYPE.largeHeader>
-          </AutoColumn>
-        </MainContentWrapper>
-      </TopSection>
+      {
+        (account) ? <TopSection gap="md">
+          <TopSection gap="2px">
+            <WrapSmall>
+              <TYPE.mediumHeader style={{ margin: '0.5rem 0.5rem 0.5rem 0', flexShrink: 0 }}>Wallet</TYPE.mediumHeader>
+            </WrapSmall>
+            <MainContentWrapper>
+              <AutoColumn gap="lg">
+                <AutoColumn gap="md" justify="center">
+                  <img src={logo} alt="Logo" style={{ width: 100, height: 100 }} />
+                </AutoColumn>
+                <AutoColumn gap="sm">
+                  <TYPE.body textAlign="center">Your $GRUMPY Balance</TYPE.body>
+                  <TYPE.largeHeader textAlign="center">{formatPrice(grumpyBalance)}</TYPE.largeHeader>
+                </AutoColumn>
+                <AutoColumn gap="sm">
+                  <TYPE.body textAlign="center">Your $GRUMPY USD Value</TYPE.body>
+                  <TYPE.largeHeader textAlign="center">{grumpyUsdValue}</TYPE.largeHeader>
+                </AutoColumn>
 
-      <TopSection gap="2px">
-        <WrapSmall>
-          <TYPE.mediumHeader style={{ margin: '0.5rem 0.5rem 0.5rem 0', flexShrink: 0 }}>Activity</TYPE.mediumHeader>
-        </WrapSmall>
-        <MainContentWrapper>
-          <AutoColumn gap="lg">
-            <AutoRow justify="center">
-              <PaddedAutoColumn gap="sm">
-                <TYPE.body textAlign="center">Total $GRUMPY In</TYPE.body>
-                <TYPE.largeHeader textAlign="center">{totalIn}</TYPE.largeHeader>
-              </PaddedAutoColumn>
+                <AutoColumn gap="sm">
+                  <AutoRow justify="center">
+                    <PaddedAutoColumn gap="sm">
+                      <TYPE.body textAlign="center">Price</TYPE.body>
+                      <TYPE.largeHeader textAlign="center">{price}</TYPE.largeHeader>
+                    </PaddedAutoColumn>
 
-              <PaddedAutoColumn gap="sm">
-                <TYPE.body textAlign="center">Total $GRUMPY Out</TYPE.body>
-                <TYPE.largeHeader textAlign="center">{totalOut}</TYPE.largeHeader>
-              </PaddedAutoColumn>
-            </AutoRow>
+                    <PaddedAutoColumn gap="sm">
+                      <TYPE.body textAlign="center">Market Cap</TYPE.body>
+                      <TYPE.largeHeader textAlign="center">{marketCap}</TYPE.largeHeader>
+                    </PaddedAutoColumn>
+                  </AutoRow>
+                </AutoColumn>
+              </AutoColumn>
+            </MainContentWrapper>
+          </TopSection>
 
-            <AutoColumn gap="sm">
-              <TYPE.body textAlign="center">Redistributed $GRUMPY</TYPE.body>
-              <TYPE.largeHeader textAlign="center">{redistributedAmount}</TYPE.largeHeader>
+          <TopSection gap="2px">
+            <WrapSmall>
+              <TYPE.mediumHeader style={{ margin: '0.5rem 0.5rem 0.5rem 0', flexShrink: 0 }}>Activity</TYPE.mediumHeader>
+            </WrapSmall>
+            <MainContentWrapper>
+              <AutoColumn gap="lg">
+                <AutoRow justify="center">
+                  <PaddedAutoColumn gap="sm">
+                    <TYPE.body textAlign="center">Total $GRUMPY In</TYPE.body>
+                    <TYPE.largeHeader textAlign="center">{formatPrice(totalIn)}</TYPE.largeHeader>
+                  </PaddedAutoColumn>
+
+                  <PaddedAutoColumn gap="sm">
+                    <TYPE.body textAlign="center">Total $GRUMPY Out</TYPE.body>
+                    <TYPE.largeHeader textAlign="center">{formatPrice(totalOut)}</TYPE.largeHeader>
+                  </PaddedAutoColumn>
+                </AutoRow>
+
+                <AutoColumn gap="sm">
+                  <TYPE.body textAlign="center">Redistributed $GRUMPY</TYPE.body>
+                  <TYPE.largeHeader textAlign="center">{formatPrice(redistributedAmount)}</TYPE.largeHeader>
+                </AutoColumn>
+
+                <AutoColumn gap="sm">
+                  <TYPE.body textAlign="center">$GRUMPY Balance without Redistribution</TYPE.body>
+                  <TYPE.largeHeader textAlign="center">{formatPrice(grumpyBalanceWithoutRedistribution)}</TYPE.largeHeader>
+                </AutoColumn>
+              </AutoColumn>
+            </MainContentWrapper>
+          </TopSection>
+        </TopSection> : <TopSection gap="2px">
+          <WrapSmall>
+            <TYPE.mediumHeader style={{ margin: '0.5rem 0.5rem 0.5rem 0', flexShrink: 0 }}>Wallet</TYPE.mediumHeader>
+          </WrapSmall>
+          <MainContentWrapper>
+            <AutoColumn gap="lg" justify="center">
+              <img src={logo} alt="Logo" style={{ width: 100, height: 100, padding: 20 }} />
             </AutoColumn>
-
             <AutoColumn gap="sm">
-              <TYPE.body textAlign="center">$GRUMPY Balance without Redistribution</TYPE.body>
-              <TYPE.largeHeader textAlign="center">{grumpyBalanceWithoutRedistribution}</TYPE.largeHeader>
+              <TYPE.body textAlign="center">Connect your wallet to see your $GRUMPY stats</TYPE.body>
             </AutoColumn>
-          </AutoColumn>
-        </MainContentWrapper>
-      </TopSection>
+          </MainContentWrapper>
+        </TopSection>}
       {/* <TYPE.subHeader color="text3">
         A minimum threshhold of 1% of the total UNI supply is required to submit proposals
       </TYPE.subHeader> */}
