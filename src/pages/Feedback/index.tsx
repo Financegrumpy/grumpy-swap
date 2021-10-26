d704b4fc8bdceb8a900823b44edcbfd9893f20da
Grumpy <github@grumpy.finance>
2021-10-26 14:56:43 -0400

    mobile styling for feedback form

--- src/pages/Feedback/index.tsx
+++ src/pages/Feedback/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { AutoColumn } from '../../components/Column'
 import styled from 'styled-components'
 import { Widget } from '@typeform/embed-react'
+import { isMobile } from 'react-device-detect'
 
 const PageWrapper = styled(AutoColumn)``
 
@@ -10,11 +11,15 @@ const TopSection = styled(AutoColumn)`
   width: 100%;
 `
 
+
 export default function Feedback () {
   return (
     <PageWrapper gap="lg" justify="center">
-      <TopSection gap="md">
-        <Widget id="wrFn178G" style={{ width:"100%", height:"550px" }}/>
+      <TopSection gap="md" style={{ 
+        width: isMobile ? '100%' : '768px',
+        height: isMobile ? '550px' : '768px'
+      }}>
+        <Widget id="wrFn178G" style={{ width:"100%", height:"100%" }}/>
       </TopSection>
     </PageWrapper>
   )
